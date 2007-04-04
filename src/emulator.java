d7832f191a4db53aca59bea9cbf93f2302ce376d
daniel.geelen <daniel.geelen@cf7422de-322d-0410-b9cf-a52b38e18328>
2007-04-04 09:52:18 +0000

    [almost]

---
+++ src/emulator.java
@@ -0,0 +1,13 @@
+class Emulator {
+  public static final void main( String[] args ) {
+    CPU cpu = new CPU("Pokemon Blue.gb");
+    TestSuite t = new TestSuite(cpu);
+
+    if(t.diagnose(true)==0) {
+      cpu.reset();
+      while(cpu.exception()==0 && cpu.cycles()<6){
+        cpu.nextinstruction();
+      }
+    }
+  }
+}
