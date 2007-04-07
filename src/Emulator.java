ffaf828031d1e12868c3e08640fda18b5d6d024d
simon.sasburg <simon.sasburg@cf7422de-322d-0410-b9cf-a52b38e18328>
2007-04-07 10:07:55 +0000

    [Debugger] yay, it can do *something*

--- src/Emulator.java
+++ src/Emulator.java
@@ -1,4 +1,40 @@
+//------------------------------------------
+// IO.java, test methods of getting user
+//    input.
+//------------------------------------------
+
 public class Emulator {
+    public static String readLn() {
+        //set aside memory for the keyboard input
+        byte [] byteArray = new byte[300];
+        
+        //things which could fail should be enclosed
+        // in a try/catch statement
+        try {
+            // read stdin into the byte array.
+            System.in.read(byteArray);
+        } catch (Exception e) {
+            // if there is a failure, print an error
+            e.printStackTrace();
+        }
+        
+        //make a String from the array of bytes
+        String result = new String(byteArray);
+
+        //check for a return character, and only
+        //keep the characters up to the return
+        int newLineIndex = result.indexOf("\n");
+        if (newLineIndex>-1)
+            result = result.substring(0,newLineIndex);
+        newLineIndex = result.indexOf("\r");
+        if (newLineIndex>-1)
+            result = result.substring(0,newLineIndex);
+        
+        return result;
+    }
+
+	
+	
 	public static final void main( String[] args ) {
 		boolean fulldebug = true;
 		for (int i = 0; i < args.length; ++i) {
@@ -17,21 +53,32 @@ public class Emulator {
 			System.out.println("Succesfully loaded ROM :)");
 			CPU cpu = new CPU(cartridge);
 			TestSuite t = new TestSuite(cpu);
+			Disassembler deasm = new Disassembler( cartridge, cpu);
 			if(true){
 				if(t.diagnose(true)==0) {
 					cpu.reset();
 					int x = 10;
+					String stopstr = "";
 					while(x > 0){
-						if (fulldebug) cpu.printCPUstatus();
+						if (fulldebug && stopstr.equals("")) {
+							cpu.printCPUstatus();
+							//System.out.println(" > " + deasm.disassemble(cpu.PC));
+							stopstr = readLn();
+						}
 						cpu.nextinstruction();
+						if (!stopstr.equals("")) {
+							String s = deasm.disassemble(cpu.PC);
+							if (s.indexOf(stopstr) > -1)
+								stopstr = "";
+						}
 						if (cpu.exception()!=0) {
-							Disassembler deasm = new Disassembler( cartridge, cpu);
 							if (!fulldebug) cpu.printCPUstatus();
 							String s = deasm.disassemble(cpu.PC);
 							if (s.charAt( 6)=='$') ++(cpu.PC);
 							if (s.charAt(10)=='$') ++(cpu.PC);
 							if (s.charAt(14)=='$') ++(cpu.PC);
 							--x;
+							stopstr = "";
 							fulldebug = true;
 						}
 					}
