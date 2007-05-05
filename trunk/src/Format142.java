0c6013748cf171c907716575f176d16363d0390e
simon.sasburg <simon.sasburg@cf7422de-322d-0410-b9cf-a52b38e18328>
2007-05-05 15:14:20 +0000

    fix format/printf for java 1.4.2 (integers only...)

---
+++ trunk/src/Format142.java
@@ -0,0 +1,15 @@
+
+class Format142 {
+	public static final String strformat(String s, int[] iarr) {
+		Object oarr[] = new Object[iarr.length];
+		for (int i = 0; i < iarr.length; ++i)
+			oarr[i] = new Integer(iarr[i]);
+		return String.format(s, oarr);
+	}
+
+	public static final void strprintf(String s, int[] iarr) {
+		s = strformat(s, iarr);
+		System.out.print(s);
+	}
+
+}
