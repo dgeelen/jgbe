db82e28dc00422d86e0a194d83bb8316b3e8fc6b
simon.sasburg <simon.sasburg@cf7422de-322d-0410-b9cf-a52b38e18328>
2007-05-15 09:27:36 +0000

    [makefile] (hopefully) fix jikes rt.jar autodetection to work more often

--- src/findrtjar.sh
+++ src/findrtjar.sh
@@ -1,4 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+testdir()
+{
+  if [ -f "$1/rt.jar" ]
+  then
+    echo "RTJARPATH := $1" >> Makefile.config
+    echo "$1/rt.jar"
+    exit
+  fi
+}
+
+#manual test cached path
+if [ -f "$1/rt.jar" ]
+then
+  echo "$1/rt.jar"
+  exit
+fi
+
 JAVALINK=`which java`
 JAVAFILE=
 while [ "${JAVALINK}" != "" ]
@@ -7,4 +25,12 @@ do
 	JAVALINK=`readlink ${JAVAFILE}`
 done
 
-echo $JAVAFILE | sed 's:/bin/[^/]*$:/lib/rt.jar:'
+TD=`echo $JAVAFILE | sed 's:/bin/[^/]*$:/lib:'`
+
+testdir "$TD"
+
+TD=`find /cygdrive/c/Program\ Files/Java/ -name rt.jar | sort | tail -n 1 | sed s:/rt\.jar$::`
+
+testdir "$TD"
+
+echo errorrtjarnotfound
