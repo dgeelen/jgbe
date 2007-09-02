a9add3c81327747211c6e3d4bb9d4fcfcc223a7d
simon.sasburg <simon.sasburg@cf7422de-322d-0410-b9cf-a52b38e18328>
2007-09-02 08:05:59 +0000

    git version detection improvement

--- src/svnversion.sh
+++ src/svnversion.sh
@@ -3,6 +3,10 @@ SVNVER=`svnversion 2> /dev/null`
 
 if [ "${SVNVER}" = "exported" ]; then
 	SVNVER=`git-svn find-rev HEAD 2> /dev/null`
+	GITDIFF=`git-diff HEAD | cat`
+	if [ "${GITDIFF}" != "" ]; then
+		SVNVER=${SVNVER}m
+	fi
 fi
 
 if [ "${SVNVER}" = "" ]; then
