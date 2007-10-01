53be3f7de0a6e5843d69b5f5b4654f63eccbb899
daniel.geelen <daniel.geelen@cf7422de-322d-0410-b9cf-a52b38e18328>
2007-10-01 11:19:12 +0000

    move makefile to . from src/
    misc makefile improvements
    add findrtjar.sh
    add version script

---
+++ findrtjar.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+testdir()
+{
+	if [ -f "$1/rt.jar" ]
+	then
+		echo "RTJARPATH := $1" >> Makefile.config
+		echo "$1/rt.jar"
+		exit
+	fi
+}
+
+#manual test cached path
+if [ -f "$1/rt.jar" ]
+then
+	echo "$1/rt.jar"
+	exit
+fi
+
+JAVALINK=`which java`
+JAVAFILE=
+while [ "${JAVALINK}" != "" ]
+do
+	JAVAFILE=${JAVALINK}
+	JAVALINK=`readlink ${JAVAFILE}`
+done
+
+TD=`echo $JAVAFILE | sed 's:/bin/[^/]*$:/lib:'`
+
+echo $TD
+
+testdir "$TD"
+
+TD=`find /cygdrive/c/Program\ Files/Java/ -name rt.jar 2> /dev/null | sort | tail -n 1 | sed s:/rt\.jar$::`
+
+testdir "$TD"
+
+echo ErrorRtJarNotFound
