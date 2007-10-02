d5232859686d561cb92f6505afc29f6556cb45cb
simon.sasburg <simon.sasburg@cf7422de-322d-0410-b9cf-a52b38e18328>
2007-10-02 00:39:49 +0000

    properly fix version script to detect svn version from within a git-svn checkout

--- svnversion.sh
+++ svnversion.sh
@@ -2,10 +2,28 @@
 SVNVER=`svnversion 2> /dev/null`
 
 if [ "${SVNVER}" = "exported" ]; then
-	SVNVER=`git-svn find-rev HEAD 2> /dev/null`
+	GITHEAD=`git-rev-parse HEAD 2> /dev/null`
+	GITHEAD_ORIG="${GITHEAD}"
+	SVNVER=
+fi
+
+if [ "${GITHEAD}" != "" ]; then
+	SVNVER=`git-svn find-rev ${GITHEAD} 2> /dev/null`
+
+	while [ \( "${SVNVER}" = "" \) -a \( "${GITHEAD}" != "" \) ]
+	do
+		GITHEAD=`git-rev-parse ${GITHEAD}^ 2> /dev/null 1> /dev/null && git-rev-parse ${GITHEAD}^ 2> /dev/null`
+		SVNVER=`git-svn find-rev ${GITHEAD} 2> /dev/null`
+#echo $GITHEAD
+	done
+
+	echo $ISGIT
+	if [ \( "${SVNVER}" != "" \) -a \( "${GITHEAD}" != "${GITHEAD_ORIG}" \) ]; then
+		SVNVER=${SVNVER}G
+	fi
 	GITDIFF=`git-diff HEAD | cat`
 	if [ "${GITDIFF}" != "" ]; then
-		SVNVER=${SVNVER}m
+		SVNVER=${SVNVER}M
 	fi
 fi
 
