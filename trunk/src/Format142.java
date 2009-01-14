6416757a885bc47d3f740754ecf6d2705069f8dd
simon.sasburg <simon.sasburg@cf7422de-322d-0410-b9cf-a52b38e18328>
2009-01-14 16:49:04 +0000

    USE_JAVA_MOBILE compiles mobile version

--- trunk/src/Format142.java
+++ trunk/src/Format142.java
@@ -4,16 +4,16 @@ class Format142 {
 		Object oarr[] = new Object[iarr.length];
 		for (int i = 0; i < iarr.length; ++i)
 			oarr[i] = new Integer(iarr[i]);
-		return String.format(s, oarr);
+		return s;//String.format(s, oarr);
 	}
 
 	public static final String strformat(String s, double[] iarr) {
 		Object oarr[] = new Object[iarr.length];
 		for (int i = 0; i < iarr.length; ++i)
 			oarr[i] = new Double(iarr[i]);
-		return String.format(s, oarr);
+		return s;//String.format(s, oarr);
 	}
-	
+
 	public static final void strprintf(String s, int[] iarr) {
 		s = strformat(s, iarr);
 		System.out.print(s);
